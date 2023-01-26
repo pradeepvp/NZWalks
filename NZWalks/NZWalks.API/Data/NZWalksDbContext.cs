02f1f4933d2465d73dda5c6173c44f6d8dba7a05
pradeepvpanzade <pvpusa73@gmail.com>
2023-01-26 15:02:11 -0500

    End of Section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("Data Source=(HFTS037LC1C60R2)\\MSSQLLocalDB;Initial Catalog=NZWalksDb");
+        //    }
+        //}
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
